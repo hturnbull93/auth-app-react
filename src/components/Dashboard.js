70faa3cd6c3f499227df9efe126ef3e083d124d5
Harry Turnbull <hturnbull@live.co.uk>
2020-05-16 15:05:55 +0100

    accepting prop and displaying it

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export const Dashboard = () => {
+export const Dashboard = (props) => {
   return (
     <div>
       <h1>Dashboard</h1>
+      <h2>Status: {props.loggedInStatus}</h2>
     </div>
-  )
+  );
 }
