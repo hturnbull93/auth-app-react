739a4ee1e434869e28c43d449b8bbc750b000764
Harry Turnbull <hturnbull@live.co.uk>
2020-05-16 18:57:49 +0100

    checkLoginStatus, pass user to dashboard as prop

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import { Dashboard } from "./Dashboard";
+import Axios from "axios";
 
 export default class App extends Component {
   constructor() {
@@ -13,12 +14,32 @@ export default class App extends Component {
     };
   }
 
+  checkLoginStatus = () => {
+    Axios.get("http://localhost:3001/logged_in", { withCredentials: true })
+      .then((res) => {
+        if (res.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN")
+          this.handleAuth(res.data);
+        else if (
+          !res.data.logged_in &&
+          this.state.loggedInStatus === "LOGGED_IN"
+        )
+          this.setState({ user: {}, loggedInStatus: "NOT_LOGGED_IN" });
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+
   handleAuth = (data) => {
     this.setState({
       user: data.user,
-      loggedInStatus: "LOGGED_IN"
-    })
+      loggedInStatus: "LOGGED_IN",
+    });
   };
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
 
   render() {
     return (
@@ -29,11 +50,7 @@ export default class App extends Component {
               exact
               path={"/"}
               render={(props) => (
-                <Home 
-                  {...props} 
-                  loggedInStatus={this.state.loggedInStatus}
-                  handleAuth={this.handleAuth}
-                />
+                <Home {...props} handleAuth={this.handleAuth} />
               )}
             />
 
@@ -41,10 +58,7 @@ export default class App extends Component {
               exact
               path={"/dashboard"}
               render={(props) => (
-                <Dashboard
-                  {...props}
-                  loggedInStatus={this.state.loggedInStatus}
-                />
+                <Dashboard {...props} user={this.state.user} />
               )}
             />
           </Switch>
