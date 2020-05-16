2fe9e7570e5beb8c79d4ed36c66a2cfcff5e545b
Harry Turnbull <hturnbull@live.co.uk>
2020-05-16 15:23:20 +0100

    handleAuth passes data up chain, redirects

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,18 @@ export class Home extends Component {
   constructor(props) {
     super(props)
   }
+
+  handleAuth = (data) => {
+    this.props.handleAuth(data)
+    this.props.history.push('/dashboard')
+  }
+
   render() {
     return (
       <div>
         <h1>Home</h1>
         <h2>Status: {this.props.loggedInStatus}</h2>
-        <Registration />
+        <Registration handleAuth={this.handleAuth}/>
       </div>
     )
   }
