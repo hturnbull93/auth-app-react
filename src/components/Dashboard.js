835b9755197e6929f372c64b67c2c74eb88220d3
Harry Turnbull <hturnbull@live.co.uk>
2020-05-16 19:22:07 +0100

    switched to class component, handleLogOut

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { Component } from "react";
+import Axios from "axios";
 
-export const Dashboard = (props) => {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <section>
-        <p>Email: {props.user.email}</p>
-        <p>Account Created At: {props.user.created_at}</p>
-      </section>
-    </div>
-  );
+export class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleLogOut = () => {
+    Axios.delete('http://localhost:3001/logout', { withCredentials: true })
+    this.props.history.push('/')
+    this.props.handleLogOut();
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        <section>
+          <h2>User Details</h2>
+          <p>Email:</p>
+          <p>{this.props.user.email}</p>
+          <p>Account Created At:</p>
+          <p>{this.props.user.created_at}</p>
+        </section>
+        <button onClick={this.handleLogOut}>Log out</button>
+      </div>
+    );
+  }
 }
+
+export default Dashboard;
